b6280c049c676d565e6364ff9edd5e76869bb020
Clara Buck <clara.buck28@gmail.com>
2020-08-07 08:54:10 -0700

    R script and png file for plot2, showing line plot of global active power over time

---
+++ plot2.R
@@ -0,0 +1,10 @@
+#fulltable <- read.csv(file = "household_power_consumption.txt", sep= ";")
+table <- subset(fulltable, Date == "1/2/2007" | Date == "2/2/2007")
+
+datetimes <- strptime(paste(table[,"Date"],table[,"Time"]), 
+                      format = c("%d/%m/%Y %H:%M:%S"))
+table[,"Date_Time"] <- data.frame(datetimes)
+png(file = "plot2.png")
+plot(table$Date_Time,table$Global_active_power, type = "l", xlab = "", 
+     ylab = "Global Active power (kilowatts)")
+dev.off()
